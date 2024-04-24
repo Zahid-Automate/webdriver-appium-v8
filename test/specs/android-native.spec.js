a6fa9bfe2e5a6831de2425edb090af49ea651c61
mzahid <abdulzahid.01@gmail.com>
2024-04-24 00:08:18 -0700

    Accept dismiss and getText of a Alert box

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -11,4 +11,28 @@ describe('Andrioid Native Feature Tests',()=>{
     // assertion
     await expect($('//*[@text="App/Alert Dialogs"]')).toExist();
     });
+
+    it.only('Click on Accept in Alert box', async()=>{
+     // access the alerts page directly
+     await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.app.AlertDialogSamples");
+
+     await $('//*[@resource-id="io.appium.android.apis:id/two_buttons"]').click();
+     
+    // accept Alert
+    // await driver.acceptAlert();
+
+    // dismiss Alert
+    // await driver.dismissAlert();
+
+    // get alert text
+    console.log('ALERT TEXT -->', await driver.getAlertText());
+
+    // click on the OK button
+    await $('//*[@resource-id="android:id/button1"]').click();
+
+    // assertion - alert box is no longer visible
+    await expect($('//*[@resource-id="android:id/alertTitle"]')).not.toExist();
+
+    });
+
 })
